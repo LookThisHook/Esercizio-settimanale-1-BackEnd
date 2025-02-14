66729501d831ebe2c7a4d8a124b8cd0d894ce0e4
LookThisHook <lucagiordano634@gmail.com>
2025-02-14 11:22:42 +0100

    Add files via upload

---
+++ Esercizio settimana backend 1/Esercizio settimana backend 1/Genere.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_settimana_backend_1
+{
+    public enum Genere
+    {
+        Maschio,
+        Femmina
+    }
+}
